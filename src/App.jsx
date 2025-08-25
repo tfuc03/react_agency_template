ea06962236813f705d7560f56adcef206821d36b
Tfuc03 <phamphucdev1n@gmail.com>
2025-08-25 21:34:27 +0700

    Update navBar, Hero + Services

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import TrustedBy from './components/TrustedBy'
+import Services from './components/Services'
+import Title from './components/Title'
+
+const App = () => {
+
+  const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
+
+  return (
+    <div className='dark:bg-black relative'>
+      <Navbar theme={theme} setTheme={setTheme}/>
+      <Hero />
+      <TrustedBy />
+      <Services />
+   
+    </div>
+
+    
+
+  )
+}
+
+export default App
